b82c4cd29a2da1c05bf562e266e54931e5717354
curttree <curttremblay@gmail.com>
2022-01-31 19:00:16 -0500

    Re-ordered animations in prep for sequences

--- Animation/Assignment1/Assignment1/cGameEngine.cpp
+++ Animation/Assignment1/Assignment1/cGameEngine.cpp
@@ -200,121 +200,113 @@ void cGameEngine::LoadAnimationAssignmentOneScene() {
 
 	// Skeleton Animations.
 	skel->animation.keyFramePositions.push_back(KeyFramePosition(0, glm::vec3(-17.f,0.f,0.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(6, glm::vec3(0.f, 0.f, 0.f),EasingType::EaseOut));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(10, glm::vec3(0.f, 0.f, 0.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(12, glm::vec3(0.f, 0.f, 8.f), EasingType::EaseInOut));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(12.75, glm::vec3(0.f, 1.f, 8.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(13, glm::vec3(0.f, 0.f, 8.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(15, glm::vec3(0.f, 0.f, 7.f), EasingType::EaseIn));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(16, glm::vec3(0.f, 0.f, 7.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(18, glm::vec3(0.f, 0.f, 6.f), EasingType::EaseIn));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(18.4, glm::vec3(0.f, 0.f, 6.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(20, glm::vec3(0.f, 0.f, 2.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(21, glm::vec3(-20.f, 0.f, 0.f)));
-	skel->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(-20.f, 0.f, 0.f)));
-
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(0, glm::quat(glm::vec3(0.f,glm::pi<float>()/2.f,0.f))));
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(8, glm::quat(glm::vec3(0.f, glm::pi<float>() / 2.f, 0.f))));
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(9, glm::quat(glm::vec3(0.f, 0.f, 0.f)), EasingType::EaseInOut));
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(18, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(18.01, glm::quat(glm::vec3(0.f, glm::pi<float>() /100.f, 0.f))));
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(18.5, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f)), EasingType::EaseOut));
-	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(30, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
-
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(0, glm::quat(glm::vec3(0.f, glm::pi<float>() / 2.f, 0.f))));
 	skel->animation.keyFrameScales.push_back(KeyFrameScale(0, 2.f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(8, 2.f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(8.5, 1.85f, EasingType::EaseInOut));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(12, 1.85f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(12.5, 3.f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(13, 2.f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(20, 2.f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(20.1, 0.f));
-	skel->animation.keyFrameScales.push_back(KeyFrameScale(30, 0.f));
 
-	// Skeleton Colour changes.
-	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(1, new cCommand_ColourChange(skelMesh,false, GetEasingColour(EasingType::EaseOut))));		// Postion
+	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(0.5, new cCommand_ConsoleOutput("<Loop Start>")));
+	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(1, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::EaseOut))));		// Postion
+	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(5.5, new cCommand_PlaySound(2)));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(6, glm::vec3(0.f, 0.f, 0.f),EasingType::EaseOut));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(6, new cCommand_ColourChange(skelMesh, true)));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(8, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::EaseInOut))));		// Rotation + Scale
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(8, 2.f));
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(8, glm::quat(glm::vec3(0.f, glm::pi<float>() / 2.f, 0.f))));
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(8.5, 1.85f, EasingType::EaseInOut));
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(9, glm::quat(glm::vec3(0.f, 0.f, 0.f)), EasingType::EaseInOut));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(9, new cCommand_ColourChange(skelMesh, true)));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(10, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::EaseInOut))));	// Position
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(10, glm::vec3(0.f, 0.f, 0.f)));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(12, glm::vec3(0.f, 0.f, 8.f), EasingType::EaseInOut));
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(12, 1.85f));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(12, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::None))));			// Position + Scale
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(12.5, 3.f));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(12.75, glm::vec3(0.f, 1.f, 8.f)));
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(13, 2.f));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(13, glm::vec3(0.f, 0.f, 8.f)));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(13, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::EaseIn))));		// Position
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(15, glm::vec3(0.f, 0.f, 7.f), EasingType::EaseIn));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(15, new cCommand_ColourChange(skelMesh, true)));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(16, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::EaseIn))));		// Position
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(16, glm::vec3(0.f, 0.f, 7.f)));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(18, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::EaseOut))));		// Rotation
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(18, glm::vec3(0.f, 0.f, 6.f), EasingType::EaseIn));
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(18, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(18.01, glm::quat(glm::vec3(0.f, glm::pi<float>() / 100.f, 0.f))));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(18.4, glm::vec3(0.f, 0.f, 6.f)));
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(18.5, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f)), EasingType::EaseOut));
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(18.5, new cCommand_ColourChange(skelMesh, false, GetEasingColour(EasingType::None))));		// Position
 	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(20, new cCommand_ColourChange(skelMesh, true)));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(20, glm::vec3(0.f, 0.f, 2.f)));
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(20, 2.f));
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(20.1, 0.f));
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(21, glm::vec3(-20.f, 0.f, 0.f)));
+	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(29.5, new cCommand_ConsoleOutput("<Loop End>")));
 
+	skel->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(-20.f, 0.f, 0.f)));
+	skel->animation.keyFrameRotations.push_back(KeyFrameRotation(30, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
+	skel->animation.keyFrameScales.push_back(KeyFrameScale(30, 0.f));
+
+	// Bush Animation
 
 	bush->animation.keyFramePositions.push_back(KeyFramePosition(0, glm::vec3(0.f, 0.f, 15.f)));
-	bush->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(0.f, 0.f, 15.f)));
-
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(0, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
+	bush->animation.keyFrameScales.push_back(KeyFrameScale(0, 0.5f));
+
+	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(3.75, new cCommand_ColourChange(bushMesh, false, GetEasingColour(EasingType::None))));		// Rotation
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(3.75f, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
+	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(4, new cCommand_PlaySound(3)));
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(4.25f, glm::quat(glm::vec3(0.f, 0.f, glm::pi<float>() / 50.f))));
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(4.75f, glm::quat(glm::vec3(0.f, 0.f, -glm::pi<float>() / 50.f))));
+	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(5.25, new cCommand_ColourChange(bushMesh, true)));
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(5.25f, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
+	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(11.2, new cCommand_PlaySound(3)));
+	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(12, new cCommand_ColourChange(bushMesh, false, GetEasingColour(EasingType::EaseIn))));		// Rotation
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(12.f, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(12.5f, glm::quat(glm::vec3(0.f, 0.f, glm::pi<float>() / 50.f)), EasingType::EaseIn));
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(13.f, glm::quat(glm::vec3(0.f, 0.f, -glm::pi<float>() / 50.f))));
 	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(13.5f, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
-	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(30.f, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
-
-	bush->animation.keyFrameScales.push_back(KeyFrameScale(0, 0.5f));
-	bush->animation.keyFrameScales.push_back(KeyFrameScale(30, 0.5f));
-
-	// Bush Colour changes.
-	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(3.75, new cCommand_ColourChange(bushMesh, false, GetEasingColour(EasingType::None))));		// Rotation
-	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(5.25, new cCommand_ColourChange(bushMesh, true)));
-	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(12, new cCommand_ColourChange(bushMesh, false, GetEasingColour(EasingType::EaseIn))));		// Rotation
 	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(13.5, new cCommand_ColourChange(bushMesh, true)));
 
+	bush->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(0.f, 0.f, 15.f)));
+	bush->animation.keyFrameRotations.push_back(KeyFrameRotation(30.f, glm::quat(glm::vec3(0.f, 0.f, 0.f))));
+	bush->animation.keyFrameScales.push_back(KeyFrameScale(30, 0.5f));
 
+	//Exclamation Animation
 	exc->animation.keyFramePositions.push_back(KeyFramePosition(0, glm::vec3(0.f, 6.f, 8.f)));
-	exc->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(0.f, 6.f, 8.f)));
-
 	exc->animation.keyFrameRotations.push_back(KeyFrameRotation(0, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
-	exc->animation.keyFrameRotations.push_back(KeyFrameRotation(30, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
-
 	exc->animation.keyFrameScales.push_back(KeyFrameScale(0, 0.f));
+
 	exc->animation.keyFrameScales.push_back(KeyFrameScale(12, 0.f));
+	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(12, new cCommand_ColourChange(excMesh, false, GetEasingColour(EasingType::EaseOut))));		// Scale
+	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(12.2, new cCommand_PlaySound(0)));
+	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(12.5, new cCommand_ColourChange(excMesh, true)));
 	exc->animation.keyFrameScales.push_back(KeyFrameScale(12.5, 5.f, EasingType::EaseOut));
 	exc->animation.keyFrameScales.push_back(KeyFrameScale(13, 5.f));
+	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(13, new cCommand_ColourChange(excMesh, false, GetEasingColour(EasingType::None))));		// Scale
+	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(13.5, new cCommand_ColourChange(excMesh, true)));
 	exc->animation.keyFrameScales.push_back(KeyFrameScale(13.5, 0.f));
+
+	exc->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(0.f, 6.f, 8.f)));
+	exc->animation.keyFrameRotations.push_back(KeyFrameRotation(30, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
 	exc->animation.keyFrameScales.push_back(KeyFrameScale(30, 0.f));
 
-	// Exclamation Colour changes.
-	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(12, new cCommand_ColourChange(excMesh, false, GetEasingColour(EasingType::EaseOut))));		// Scale
-	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(12.5, new cCommand_ColourChange(excMesh, true)));
-	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(13, new cCommand_ColourChange(excMesh, false, GetEasingColour(EasingType::None))));		// Scale
-	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(13.5, new cCommand_ColourChange(excMesh, true)));	
-
+	//Blood Animation
 	blood->animation.keyFramePositions.push_back(KeyFramePosition(0, glm::vec3(0.f, 3.f, 5.f)));
-	blood->animation.keyFramePositions.push_back(KeyFramePosition(19, glm::vec3(0.f, 3.f, 5.f)));
-	blood->animation.keyFramePositions.push_back(KeyFramePosition(20, glm::vec3(0.f, 3.f, -10.f)));
-	blood->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(0.f, -10.f, -10.f), EasingType::EaseOut));
-
 	blood->animation.keyFrameRotations.push_back(KeyFrameRotation(0, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
-	blood->animation.keyFrameRotations.push_back(KeyFrameRotation(30, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
-
 	blood->animation.keyFrameScales.push_back(KeyFrameScale(0, 0.f));
-	blood->animation.keyFrameScales.push_back(KeyFrameScale(19, 0.f));
-	blood->animation.keyFrameScales.push_back(KeyFrameScale(20.5, 20.f, EasingType::EaseIn));
-	blood->animation.keyFrameScales.push_back(KeyFrameScale(30, 20.f));
 
-	// Blood Colour changes.
 	blood->animation.keyFrameEvents.push_back(KeyFrameEvent(1, new cCommand_ColourChange(bloodMesh, false, GetEasingColour(EasingType::None))));		// Position
+	blood->animation.keyFrameEvents.push_back(KeyFrameEvent(18.5, new cCommand_PlaySound(1)));
+	blood->animation.keyFrameScales.push_back(KeyFrameScale(19, 0.f));
+	blood->animation.keyFramePositions.push_back(KeyFramePosition(19, glm::vec3(0.f, 3.f, 5.f)));
 	blood->animation.keyFrameEvents.push_back(KeyFrameEvent(19, new cCommand_ColourChange(bloodMesh, false, GetEasingColour(EasingType::EaseIn))));		//Scale
+	blood->animation.keyFramePositions.push_back(KeyFramePosition(20, glm::vec3(0.f, 3.f, -10.f)));
+	blood->animation.keyFrameScales.push_back(KeyFrameScale(20.5, 20.f, EasingType::EaseIn));
 	blood->animation.keyFrameEvents.push_back(KeyFrameEvent(20.5, new cCommand_ColourChange(bloodMesh, false, GetEasingColour(EasingType::EaseOut))));		//Position
 
-	// Additional Events
-	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(0.5, new cCommand_ConsoleOutput("<Loop Start>")));
-	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(4, new cCommand_PlaySound(3)));
-	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(5.5, new cCommand_PlaySound(2)));
-	bush->animation.keyFrameEvents.push_back(KeyFrameEvent(11.2, new cCommand_PlaySound(3)));
-	exc->animation.keyFrameEvents.push_back(KeyFrameEvent(12.2, new cCommand_PlaySound(0)));
-	blood->animation.keyFrameEvents.push_back(KeyFrameEvent(18.5, new cCommand_PlaySound(1)));
-	skel->animation.keyFrameEvents.push_back(KeyFrameEvent(29.5, new cCommand_ConsoleOutput("<Loop End>")));
+	blood->animation.keyFramePositions.push_back(KeyFramePosition(30, glm::vec3(0.f, -10.f, -10.f), EasingType::EaseOut));
+	blood->animation.keyFrameRotations.push_back(KeyFrameRotation(30, glm::quat(glm::vec3(0.f, glm::pi<float>(), 0.f))));
+	blood->animation.keyFrameScales.push_back(KeyFrameScale(30, 20.f));
 }
 
 void cGameEngine::SwapPlayer(unsigned int newPlayerNum) {
